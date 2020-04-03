79f68cbd33d461d068a326aa8423fbd06cbcab9a
dhanush131997 <dhanushvenkatesh176@gmail.com>
2020-04-03 14:45:02 +0530

    airlineadmin

---
+++ src/main/java/com/capgemini/airlinereservationsystemadmin/AirlinereservationsystemadminApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.airlinereservationsystemadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinereservationsystemadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinereservationsystemadminApplication.class, args);
+	}
+
+}
